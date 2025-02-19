6003ab31f274c64bb34ac740571c05145846d659
nero31994 <arenaofvalorph937@gmail.com>
2025-02-20 06:29:59 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
                     </style>
                 </head>
                 <body>
-                    <iframe src="${embedUrl}" allowfullscreen></iframe>
+                    <iframe src="${embedUrl}" autofullscreen></iframe>
                 </body>
                 </html>
             `);
